e110325fcf6ab29743fb0557916d24cb67c47cd0
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-04-17 00:07:39 +0200

    Add hero section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/sections/hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HeroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
