f4446320daf8064cbb4843581ac04c3b6d169e16
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-04-17 21:14:43 +0200

    Styles header behaviour on scroll, replaces nav links list by mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from './components/sections/hero/hero.component';
-import { AboutComponent } from './components/sections/about/about.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from "./components/sections/hero/hero.component";
+import { AboutComponent } from "./components/sections/about/about.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HeroComponent, AboutComponent],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
+  sections = [
+    {
+      title: "О проекте",
+      anchor: "about_project",
+    },
+    {
+      title: "Квартиры на 3D плане",
+      anchor: "apartments_plan",
+    },
+    {
+      title: "Квартиры по параметрам",
+      anchor: "apartments_parameters",
+    },
+  ];
+  activeLink: string = "";
+  compactHeader = false;
 
+  fakeRoute(event: MouseEvent): void {
+    this.activeLink = (event.target as HTMLElement).dataset["route"] || "";
+  }
+
+  @HostListener("window:scroll")
+  private onWindowScroll() {
+    const scrollTop = window.scrollY;
+
+    this.compactHeader = scrollTop > 100;
+  }
 }
