bfde4ef565a65b945699a0aea1266d071dfb6949
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-05-18 19:30:58 +0200

    add ngSrc attribute for images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/sections/hero/hero.component";
 import { AboutComponent } from "./components/sections/about/about.component";
-import { PromotionsAndNewsComponent } from './components/sections/promotions-and-news/promotions-and-news.component';
-import { TabsComponent } from './components/tabs/tabs.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LocationComponent } from './components/sections/location/location.component';
+import { PromotionsAndNewsComponent } from "./components/sections/promotions-and-news/promotions-and-news.component";
+import { TabsComponent } from "./components/tabs/tabs.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { LocationComponent } from "./components/sections/location/location.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroComponent, AboutComponent, PromotionsAndNewsComponent, TabsComponent, FooterComponent, LocationComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    PromotionsAndNewsComponent,
+    TabsComponent,
+    FooterComponent,
+    LocationComponent,
+  ],
+  imports: [BrowserModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
 })
