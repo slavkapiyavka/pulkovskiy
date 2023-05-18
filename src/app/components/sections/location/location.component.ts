937628ebbd3815e71a5dac36a9495d1a919fa8cc
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-05-18 19:30:40 +0200

    add map in the 'location' component

--- src/app/components/sections/location/location.component.ts
+++ src/app/components/sections/location/location.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Tab } from "../../../../shared/interfaces/tab";
+
+declare const ymaps: any;
 
 @Component({
-  selector: 'app-location',
-  templateUrl: './location.component.html',
-  styleUrls: ['./location.component.scss']
+  selector: "app-location",
+  templateUrl: "./location.component.html",
+  styleUrls: ["./location.component.scss"],
 })
-export class LocationComponent {
+export class LocationComponent implements OnInit {
+  map: any;
+  latitude = 52.354225;
+  longitude = 104.224625;
+  zoom = 17;
+  @ViewChild("yamaps")
+  mapElement!: ElementRef;
 
+  public tabs: Tab[] = [
+    {
+      name: "Все",
+      link: "all",
+    },
+    {
+      name: "Рестораны",
+      link: "all",
+    },
+    {
+      name: "Музеи",
+      link: "all",
+    },
+    {
+      name: "Магазины",
+      link: "all",
+    },
+    {
+      name: "Больницы",
+      link: "all",
+    },
+    {
+      name: "Спорт",
+      link: "all",
+    },
+    {
+      name: "Театр",
+      link: "all",
+    },
+    {
+      name: "Ж/Д вокзал",
+      link: "all",
+    },
+  ];
+
+  ngOnInit() {
+    ymaps.ready().done(() => this.createMap());
+  }
+
+  public setActiveLocationType(type: string): void {
+    console.log(type);
+  }
+
+  private createMap(): void {
+    this.map = new ymaps.Map("map", {
+      center: [this.latitude, this.longitude],
+      zoom: this.zoom,
+      controls: [],
+    });
+
+    const placeMark = new ymaps.Placemark([this.latitude, this.longitude]);
+    this.map.geoObjects.add(placeMark);
+  }
 }
